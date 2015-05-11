b53ac1851f8298bf5986fa00b5249b80ca9ebac6
Johan Nyholm <hansjohannyholm@gmail.com>
2015-05-11 23:07:47 +0200

    started editing the buddyHM

--- mallocBS.c
+++ mallocBS.c
@@ -1,1 +1,60 @@
-mallocBS.c
+#include "mallocBS.h"
+
+
+
+
+static block_t* free_list[N];
+static char memory[POOL_SIZE];
+
+
+block_t* find_free_block(size_t size)
+{
+	size_t req_kval=two_exp(size);
+	block_t* block=NULL;
+	size_t index=req_kval;
+	while(index<N){
+		if(free_list[index]!=NULL){
+			return split_block(block,req_kval);
+		}
+		index++;
+	}
+	return NULL;
+}
+
+block_t* split_block(block_t* block,size_t req_kval)
+{
+	size_t current_kval=block->kval;
+	char* byte_ptr=(char*)block;
+	while(current_kval > req_kval){
+		current_kval--;
+		
+		block_t* right_half=(block_t*)(byte_ptr + two_to_pow(current_kval));
+		
+	}
+}
+size_t two_to_pow(size_t pow)
+{
+	return 1<<pow;
+}
+size_t two_exp(size_t value)
+{
+	size_t ONE=0x1;
+	
+	size_t nbits=0;
+	size_t nones=0;
+
+	while(value>0){
+		nbits++;
+		if(value & ONE == ONE){
+			nones++;
+		}
+		value>>=1;
+	}
+	return (nones>1) ? nbits : nbits-1;
+}
+
+int main()
+{
+	size_t n=4;
+	printf("%zd\n",two_exp(n));
+}
---
+++ mallocBS.h
@@ -0,0 +1,39 @@
+#define _BSD_SOURCE
+
+#include <stddef.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+#define N (23)
+#define POOL_SIZE (8*1024*1024)
+
+
+typedef struct block_t block_t;
+
+struct block_t{
+	bool reserved; /* one if reserved. */
+	char kval; /* current value of K. */
+	block_t* succ; /* successor block in list. */
+	block_t* pred; /* predecessor block in list. */
+};
+
+#define BLOCK_SIZE (sizeof(block_t))
+block_t* find_free_block(size_t size);
+
+
+
+void* realloc(void* ptr, size_t size);
+void* malloc(size_t size); 
+void* calloc(size_t nitems, size_t size); 
+void free(void* ptr);
+
+
+//private 
+size_t two_exp(size_t value);
+size_t two_to_pow(size_t pow);
+block_t* split_block(block_t* block,size_t req_kval);
